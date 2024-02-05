9637cc830c0d7265d1e30abcd7feb5301952cdd9
RabieImghi <rabieaitimghi7@gmail.com>
2024-02-05 10:13:06 +0100

    add aside bare with responsive

--- fille-rouge-vue/src/components/AppAside.vue
+++ fille-rouge-vue/src/components/AppAside.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <aside class="bg-white pt-4">
+    <div class="asideBare">
+        <aside class="bg-white pt-4" id="aside">
             <section class="addQuesion container-mf ">
                 <router-link to="/AskQuesion" class="btn btn-primary w-100 fw-bold">Ask A Question</router-link>
             </section>
@@ -65,18 +65,63 @@
                 </div>
                 
             </section>
+            <section class="topMembers container-mf border-top mt-4 pt-4">
+                <div class="headerMembers d-flex gap-3 align-items-end">
+                    <img src="../assets/img/tage.png" width="45px" alt="User">
+                    <span class="h3 text-color-primary fw-bold">Trending Tages</span>   
+                </div>
+                <div class="BodMembers d-flex flex-wrap justify-content-between px-2 mt-3">
+                    <div class="user d-flex gap-2 justify-content-between align-items-center flex-wrap border mt-2 p-2">
+                        <img src="../assets/img/tage.png" width="40px" alt="">
+                        <div class="infoUser fw-bold d-flex flex-column align-items-center">
+                            <span class="color-premary">Tage_Name</span>
+                        </div>
+                    </div>
+                    <div class="user d-flex gap-2 justify-content-between align-items-center flex-wrap border mt-2 p-2">
+                        <img src="../assets/img/tage.png" width="40px" alt="">
+                        <div class="infoUser fw-bold d-flex flex-column align-items-center">
+                            <span class="color-premary">Tage_Name</span>
+                        </div>
+                    </div>
+                    <div class="user d-flex gap-2 justify-content-between align-items-center flex-wrap border mt-2 p-2">
+                        <img src="../assets/img/tage.png" width="40px" alt="">
+                        <div class="infoUser fw-bold d-flex flex-column align-items-center">
+                            <span class="color-premary">Tage_Name</span>
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
         </aside>
+        <div class="togleButton d-flex justify-content-end pe-4 pt-3">
+            <img @click="toggleAside" src="../assets/img/menu.png" class="p-1 shadow rounded-1" style="background: white;" id="togleButton" width="50px" alt="togleMenu">
+        </div>
     </div>
 </template>
 <script>
     export default{
         name: 'AppAside',
+        methods: {
+            toggleAside() {
+                const aside = document.getElementById('aside');
+                aside.classList.toggle('show-aside');
+            },
+        },
     }
 </script>
 <style>
     aside{
         height: calc(100vh - 80px);
+        overflow-y: scroll;
         box-shadow: 5px 10px 20px rgba(128, 128, 128, 0.245);
+        transition: transform 0.3s ease;
+    }
+    aside::-webkit-scrollbar{
+        display: none;
+    }
+    .show-aside {
+        display: block !important;
+        transform: translateX(0);
     }
     .col-6{
         padding-left: 0px !important;
@@ -84,5 +129,18 @@
     }
     .contentColumn{
         border-left: 4px solid ;
+    }
+    .togleButton{
+        display: none !important;
+        z-index: 1 !important;
+        position: relative
+    }
+    @media screen and (max-width: 1250px) {
+        .togleButton{
+            display: flex !important;
+        }
+        aside {
+        transform: translateX(-100%);
+        }
     }
 </style>
