152e5504ef76d356772e4b2acc174ad4fc339a33
RabieImghi <rabieaitimghi7@gmail.com>
2024-02-17 23:33:22 +0100

    add page vide for router and fixe same error in icons link

--- fille-rouge-vue/src/routes/routes.js
+++ fille-rouge-vue/src/routes/routes.js
@@ -1,34 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import AppHome from '../components/componentsHome/AppHome.vue';
-import AppBadge from '../components/componentsService/AppBadge.vue';
-import AppUsers from '../components/componentsService/AppUsers.vue';
-import AppTages from '../components/componentsService/AppTages.vue';
-import AppContact from '../components/componentsService/AppContact.vue';
-import AppProfile from '../components/componrntsProfile/AppProfile.vue';
-import AppProfileSetting from '../components/componrntsProfile/AppProfileSetting.vue';
-import AppMyQuistion from '../components/componrntsProfile/AppMyQuestion.vue';
-import AppAnswers from '../components/componentsHome/AppAnswers.vue';
-import AppAboutUs from '../components/componentsHome/AppAboutUs.vue';
-import AppAskQuesion from '../components/componentsHome/AppAskQuesion.vue';
+
+
+import AppUser from '../components/User/AppUser.vue';
+import AppHome from '../components/User/componentsHome/AppHome.vue';
+import AppBadge from '../components/User/componentsService/AppBadge.vue';
+import AppUsers from '../components/User/componentsService/AppUsers.vue';
+import AppTages from '../components/User/componentsService/AppTages.vue';
+import AppContact from '../components/User/componentsService/AppContact.vue';
+import AppProfile from '../components/User/componrntsProfile/AppProfile.vue';
+import AppProfileSetting from '../components/User/componrntsProfile/AppProfileSetting.vue';
+import AppMyQuistion from '../components/User/componrntsProfile/AppMyQuestion.vue';
+import AppAnswers from '../components/User/componentsHome/AppAnswers.vue';
+import AppAboutUs from '../components/User/componentsHome/AppAboutUs.vue';
+import AppAskQuesion from '../components/User/componentsHome/AppAskQuesion.vue';
+
+
+import AppAdmin from '../components/Admin/AppAdmin.vue';
+import AppAdminHome from '../components/Admin/AppAdminHome.vue';
+import AppAdminUsers from '../components/Admin/AppAdminUsers.vue';
 
 const routes = [
-    {name: 'Home', path: '/', component: AppHome},
-    {name: 'Servises/Badges', path: '/Servises/Badges', component: AppBadge},
-    {name: 'Servises/Users', path: '/Servises/Users', component: AppUsers},
-    {name: 'Servises/Tages', path: '/Servises/Tages', component: AppTages},
-    {name: '/ContactUs', path: '/ContactUs', component: AppContact},
-    {name: '/Profile', path: '/Profile', component: AppProfile},
-    {name: '/Settings', path: '/Settings', component: AppProfileSetting},
-    {name: '/MyQuistion', path: '/MyQuistion', component: AppMyQuistion},
-    {name: '/Answers', path: '/Answers', component: AppAnswers},
-    {name: '/AboutUs', path: '/AboutUs', component: AppAboutUs},
-    {name: '/AskQuesion', path: '/AskQuesion', component: AppAskQuesion},
-    
+    { path: '', redirect: '/user/' },
+    { 
+        path: '/user',
+        component: AppUser,
+        children: [
+            { path: '', component: AppHome },
+            { path: 'Servises/Badges', component: AppBadge },
+            { path: 'Servises/Users', component: AppUsers },
+            { path: 'Servises/Tages', component: AppTages },
+            { path: 'contactUs', component: AppContact },
+            { path: 'profile', component: AppProfile },
+            { path: 'settings', component: AppProfileSetting },
+            { path: 'myQuestion', component: AppMyQuistion },
+            { path: 'answers', component: AppAnswers },
+            { path: 'aboutUs', component: AppAboutUs },
+            { path: 'AskQuesion', component: AppAskQuesion },
+        ]
+    },
+    { 
+        path: '/admin',
+        component: AppAdmin,
+        children: [
+            { path: '', component: AppAdminHome },
+            { path: 'users', component: AppAdminUsers },
+        ]
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
-})
+});
 
 export default router;
