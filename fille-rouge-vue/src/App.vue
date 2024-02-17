64edf2f7b8c19fcb5e4095ebb032cb18946b4f16
RabieImghi <rabieaitimghi7@gmail.com>
2024-02-17 23:31:13 +0100

    add admin template and fixe router link between admin and user

--- fille-rouge-vue/src/App.vue
+++ fille-rouge-vue/src/App.vue
@@ -1,43 +1,11 @@
 <template>
   <div class="myApp">
-    <AppLayout/>
-    <div class="d-flex contentApp">
-      <div class=" AppContentBody">
-        <router-view></router-view> 
-      </div>
-      <div class="aside">
-        <AppAside/>
-      </div>
-    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import AppLayout from './components/AppLayout.vue'
-import AppAside from './components/AppAside.vue'
 export default {
-  name: 'App',
-  components: {
-    AppLayout,
-    AppAside,
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-  .aside{
-    padding: 0 !important;
-    width: 25%;
-  }
-  .AppContentBody{
-    height: calc(100vh - 80px);
-    overflow-y: scroll;
-    width: 75%;
-  }
-  .AppContentBody::-webkit-scrollbar{
-    display: none !important;
-  }
-  .contentApp{
-    margin: 0 !important;
-  }
-</style>
