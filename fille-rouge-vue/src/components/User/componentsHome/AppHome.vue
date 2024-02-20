5201962b24858563747f2ca21265c5436fc38a31
RabieImghi <rabieaitimghi7@gmail.com>
2024-02-20 15:30:33 +0100

    add badge Systeme display and add form login and registre

--- fille-rouge-vue/src/components/User/componentsHome/AppHome.vue
+++ fille-rouge-vue/src/components/User/componentsHome/AppHome.vue
@@ -6,7 +6,7 @@
                 <p class="text-secondary">We want to connect the people who have knowledge to the people who need it, to bring 
                 together people with different perspectives so they can understand each other better,
                 and to empower everyone to share their knowledge.</p>
-                <router-link to='/SignUp' class="btn btn-primary w-50">Create A New Account</router-link>
+                <router-link to='auth/' class="btn btn-primary w-50">Create A New Account</router-link>
             </div>
             <img src="../../../assets/img/auth.png" id="imageAuth" width="40%" class="p-3" alt="Image">
         </section>
